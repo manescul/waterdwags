87360e55381260b9432c9c5c40e40562fb77f706
Lucian Manescu <lmanescu@gmail.com>
2023-06-25 19:29:39 +0200

    Add the adddog route with controller action and storing job

--- app/Http/Controllers/DogController.php
+++ app/Http/Controllers/DogController.php
@@ -2,9 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\StoreDogDataJob;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class DogController extends Controller
 {
-    //
+    protected const DOG_NAME = 'name';
+    protected const DOG_MANDATORY_FIELDS = [
+        self::DOG_NAME,
+    ];
+
+    public function addDog(Request $request): Response
+    {
+        $dogData = $request->json()->all();
+
+        if (!$this->isDogDataValid($dogData)) {
+            return abort(422);
+        }
+
+        StoreDogDataJob::dispatch($dogData)->afterResponse();
+
+        return response('', 200);
+    }
+
+    protected function isDogDataValid(array $dogData): bool
+    {
+        foreach (self::DOG_MANDATORY_FIELDS as $field) {
+            if(!isset($dogData[$field])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
---
+++ app/Jobs/StoreDogDataJob.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Jobs;
+
+use Database\Factories\DogFactory;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class StoreDogDataJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(protected array $dogData)
+    {}
+
+    public function handle(): void
+    {
+        DogFactory::new()
+            ->createOne(
+                ['data' => json_encode($this->dogData)]
+            );
+    }
+}
