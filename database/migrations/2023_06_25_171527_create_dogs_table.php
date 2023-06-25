c95f2426224bf38e761607908a05bcadb9582d01
Lucian Manescu <lmanescu@gmail.com>
2023-06-25 19:35:43 +0200

    Update Dog migration

--- database/migrations/2023_06_25_171527_create_dogs_table.php
+++ database/migrations/2023_06_25_171527_create_dogs_table.php
@@ -6,20 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('dogs', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->jsonb('data')->index();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('dogs');
